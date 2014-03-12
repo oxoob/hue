240237c4eded110223b04dd890649f405426e480
Oxoob <git@oxoob.com>
2014-03-13 00:41:28 +0100

    updated sql to reflect current situation

--- create_tables.sql
+++ create_tables.sql
@@ -1,27 +1,57 @@
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
-CREATE TABLE IF NOT EXISTS `lights` (
-`primary_key` varchar(32) NOT NULL,
-`datetime` datetime NOT NULL,
-`light` tinyint(4) NOT NULL,
-`set_bri` smallint(6),
-`set_hue` mediumint(9),
-`set_sat` smallint(6),
-`current_bri` smallint(6),
-`current_hue` mediumint(9),
-`current_sat` smallint(6),
-PRIMARY KEY (`primary_key`)
+-- Database: `hued`
+-- Table structure for table `lights_adjusted`
+DROP TABLE IF EXISTS `lights_adjusted`;
+CREATE TABLE IF NOT EXISTS `lights_adjusted` (
+  `primary_key` varchar(32) NOT NULL,
+  `time` time NOT NULL,
+  `light` tinyint(4) NOT NULL,
+  `adjusted_bri` mediumint(9) NOT NULL DEFAULT '0',
+  `adjusted_hue` mediumint(9) NOT NULL DEFAULT '0',
+  `adjusted_sat` mediumint(9) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`primary_key`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- Table structure for table `lights_scheduled`
+DROP TABLE IF EXISTS `lights_scheduled`;
+CREATE TABLE IF NOT EXISTS `lights_scheduled` (
+  `primary_key` varchar(32) NOT NULL,
+  `time` time NOT NULL,
+  `light` tinyint(4) NOT NULL,
+  `scheduled_bri` mediumint(9) NOT NULL DEFAULT '0',
+  `scheduled_hue` mediumint(9) NOT NULL DEFAULT '0',
+  `scheduled_sat` mediumint(9) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`primary_key`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Table structure for table `lights_smoothed`
+DROP TABLE IF EXISTS `lights_smoothed`;
+CREATE TABLE IF NOT EXISTS `lights_smoothed` (
+  `primary_key` varchar(32) NOT NULL,
+  `time` time NOT NULL,
+  `light` tinyint(4) NOT NULL,
+  `smoothed_bri` mediumint(9) NOT NULL DEFAULT '0',
+  `smoothed_hue` mediumint(9) NOT NULL DEFAULT '0',
+  `smoothed_sat` mediumint(9) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`primary_key`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Table structure for table `sun`
+DROP TABLE IF EXISTS `sun`;
 CREATE TABLE IF NOT EXISTS `sun` (
-`datetime` datetime NOT NULL,
-`percentage` tinyint(4),
-PRIMARY KEY (`datetime`)
+  `time` time NOT NULL,
+  `percentage` tinyint(4) NOT NULL,
+  PRIMARY KEY (`time`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- Table structure for table `weather`
+DROP TABLE IF EXISTS `weather`;
 CREATE TABLE IF NOT EXISTS `weather` (
-`datetime` datetime NOT NULL,
-`percentage` tinyint(4),
-PRIMARY KEY (`datetime`)
+  `time` time NOT NULL,
+  `percentage` tinyint(4) NOT NULL,
+  PRIMARY KEY (`time`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- Remove old table
+DROP TABLE IF EXISTS `lights`;
